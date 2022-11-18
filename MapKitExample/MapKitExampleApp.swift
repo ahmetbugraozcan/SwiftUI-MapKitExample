582b2d7c3499f127bead70edd4e27efceeb1a443
ahmetbugraozcan <bugra.ahmetozcan@gmail.com>
2022-11-18 22:36:52 +0300

    Initial Commit

---
+++ MapKitExample/MapKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitExampleApp.swift
+//  MapKitExample
+//
+//  Created by Ahmet Buğra Özcan on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
